b3c83acb9f712b9286b42ead8d708749f150d1e0
Dominic <ichadominic5@gmail.com>
2026-03-14 11:14:58 +0100

    Updated homepage image and styles

--- ICHASON_PROJ/settings.py
+++ ICHASON_PROJ/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # config = Config(RepositoryEnv(BASE_DIR / ".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-q#*ec$1wu*ql#8put5he%4j7pd7*x@o_d4#$coei+s39gw^=^5")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
