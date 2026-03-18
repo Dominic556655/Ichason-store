9e62bd4ce3612cb4e8d68febc7ee91cdada2e953
Dominic <ichadominic5@gmail.com>
2026-03-18 13:15:15 +0100

    Update settings.py

--- ICHASON_PROJ/settings.py
+++ ICHASON_PROJ/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-q#*ec$1wu*ql#8put5he%4j7pd7*x@o_d4#$coei+s39gw^=^5")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,ichason-proj.onrender.com").split(",")
 
