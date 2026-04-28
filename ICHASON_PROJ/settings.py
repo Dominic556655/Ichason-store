20e34ceccc63e66b10da0d38149a2c125d37eca0
Dominic <ichadominic5@gmail.com>
2026-04-28 21:28:36 +0100

    Restore settings.py with initial configuration

--- ICHASON_PROJ/settings.py
+++ ICHASON_PROJ/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,ichason-proj.onrender.com").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,ichason-store.onrender.com").split(",")
 
 SOCIALACCOUNT_LOGIN_ON_GET=True
 # Application definition
